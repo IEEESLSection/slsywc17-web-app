dac64aeb663f2187620398ccdae0a36d8fe8c387
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-09-25 08:49:53 +0530

    Countdown added.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 particlesJS.load('particles-js', 'particles.json', function() {
  console.log('particles.js loaded - callback');
  });
+
+// var clock = $('.the-countdown').FlipClock({
+// // ... your options here
+// });
+
+var clock;
+
+$(document).ready(function() {
+
+    // Grab the current date
+    var currentDate = new Date();
+
+    // Set some date in the future. In this case, it's always Jan 1
+    var futureDate  = new Date(currentDate.getFullYear(), 11, 23);
+
+    // Calculate the difference in seconds between the future and current date
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+    // Instantiate a coutdown FlipClock
+    clock = $('.the-countdown').FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        countdown: true
+    });
+});
